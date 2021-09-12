3f563267061e2fd78e53cd2cc63007bac339fe47
Vipin <vipinrao03@gmail.com>
2021-09-12 14:29:51 +0530

    done4

--- jscode/main.js
+++ jscode/main.js
@@ -2,7 +2,7 @@ function menubar(){
     var SituationTeller = document.getElementById("arrowicon");
     SituationTeller.classList.remove("fa-caret-right");
     SituationTeller.classList.add("fa-caret-down");
-    document.getElementById("currentheading").innerHTML = currentSituation;
+    // document.getElementById("currentheading").innerHTML = currentSituation;
     document.getElementById("mobi-menubar").style.display = "block";
     document.getElementById("showmenu").style.display = "none";
     document.getElementById("hidemenu").style.display = "block";
@@ -11,7 +11,7 @@ function hide_menubar(){
     var SituationTeller = document.getElementById("arrowicon");
     SituationTeller.classList.add("fa-caret-right");
     SituationTeller.classList.remove("fa-caret-down");
-    document.getElementById("currentheading").innerHTML = currentSituation;
+    // document.getElementById("currentheading").innerHTML = currentSituation;
     document.getElementById("mobi-menubar").style.display = "none";
     document.getElementById("showmenu").style.display = "block";
     document.getElementById("hidemenu").style.display = "none";

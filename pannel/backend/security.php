b5b874d85f3a1b82ee836e09abea3f8790a4b53c
Vipin <vipinrao03@gmail.com>
2021-09-23 22:33:41 +0530

    cwefwqe

--- pannel/backend/security.php
+++ pannel/backend/security.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script src="../pannel_js/features.js"></script>
+    <script src="feature.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
         body {
